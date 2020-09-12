b16c5f3d5cfc659515b524fa383152a4e773001c
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-09-12 13:54:51 +0800

    ADd script to scrape toilets in Singapore

---
+++ backend/scripts/scrape.py
@@ -0,0 +1,46 @@
+import urllib.request, urllib.error, urllib.parse
+import json
+BASE_URL = 'http://toilet.org.sg/'
+
+
+def get_images_url(url):
+    response = urllib.request.urlopen(url)
+    images_url_result = []
+    soup = BeautifulSoup(response.read())
+    all_images_element = soup.select('img')
+    for image_element in all_images_element:
+        image_url = BASE_URL + image_element.get('src')
+        images_url_result.append(image_url)
+    return images_url_result
+
+
+from bs4 import BeautifulSoup
+toilets = []
+soup = BeautifulSoup(open("toilets.html", "r"))
+all_tables = soup.select("table")
+for table in all_tables:
+    all_rows = table.select('tbody')[0].select('tr')
+    for row in all_rows:
+        all_columns = row.select('td')
+        link_images = all_columns[1].select('a')
+        if link_images:
+
+            name = link_images[0].contents[-1].strip()
+            url_to_image_page = link_images[0].get('href')
+            images_url = get_images_url(url_to_image_page)
+        else:
+            name = all_columns[1].contents[0]
+            images_url = []
+        address = all_columns[2].contents[0]
+        stars = len(all_columns[3].select('i'))
+        toilets.append({
+            'name': name,
+            'address': address,
+            'stars': stars,
+            'images_url': images_url
+        })
+
+print(json.dumps(
+        toilets,
+        indent=4,
+    ))
