1bb00a76e0a54a9415c0ff2a5e8aa09287d31844
Jin Ying <kenneth.jyt@gmail.com>
2020-09-15 08:59:55 +0800

    Setup framwork7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Framework7 from 'framework7/framework7.esm.bundle';
+import Framework7React from 'framework7-react';
+import 'framework7/css/framework7.bundle.css';
+
+Framework7.use(Framework7React);
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
