39da1b5992dce9356d35116ab38cfdbabc770c32
Jin Ying <kenneth.jyt@gmail.com>
2020-09-16 19:03:47 +0800

    Setup framework7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
 import Framework7 from 'framework7/framework7.esm.bundle';
 import Framework7React from 'framework7-react';
 import 'framework7/css/framework7.bundle.css';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 Framework7.use(Framework7React);
 
