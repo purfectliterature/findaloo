93d617f9aeaa32380c4f3d6e1d9303b635ee7f22
Jiang Yuxin <jyx11011@gmail.com>
2020-09-18 14:43:30 +0800

    update

--- frontend/src/ListPage/ListPage.js
+++ frontend/src/ListPage/ListPage.js
@@ -5,6 +5,7 @@ import Card from "../Card/Card.js";
 import FloatButton from "../FloatButton/FloatButton.js";
 import MapIcon from "@material-ui/icons/Map";
 import Masonry from "react-masonry-css";
+import Page from "framework7-react";
 
 const breakpointColumnsObj = {
     default: 4,
@@ -38,47 +39,37 @@ const toilets = [
     toilet,
 ];
 
-class ListPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            //toiletList: props.toiletList,
-            toiletList: toilets,
-        };
+function renderToiletList() {
+    let list = [];
+    for (const toilet of toilets) {
+        list.push(<Card toilet={toilet} key={list.length}></Card>);
     }
-
-    renderToiletList() {
-        let list = [];
-        for (const toilet of this.state.toiletList) {
-            list.push(<Card toilet={toilet} key={list.length}></Card>);
-        }
-        return list;
-    }
-
-    render() {
-        return (
-            <div className="list-page">
-                <div className="searchBar-container">
-                    <SearchBar></SearchBar>
-                </div>
-                <div className="title-container">
-                    <h3>Is nature calling now?</h3>
-                </div>
-                <Masonry
-                    breakpointCols={breakpointColumnsObj}
-                    className="my-masonry-grid"
-                    columnClassName="my-masonry-grid_column"
-                    id="toilet-list"
-                >
-                    {this.renderToiletList()}
-                </Masonry>
-                <FloatButton
-                    text="View Map"
-                    icon={<MapIcon></MapIcon>}
-                ></FloatButton>
-            </div>
-        );
-    }
+    return list;
 }
+
+export const ListPage = () => (
+    <Page>
+        <div className="list-page">
+            <div className="searchBar-container">
+                <SearchBar></SearchBar>
+            </div>
+            <div className="title-container">
+                <h3>Is nature calling now?</h3>
+            </div>
+            <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+                id="toilet-list"
+            >
+                {renderToiletList()}
+            </Masonry>
+            <FloatButton
+                text="View Map"
+                icon={<MapIcon></MapIcon>}
+            ></FloatButton>
+        </div>
+    </Page>
+);
 
 export default ListPage;
