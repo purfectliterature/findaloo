e6d91c1c7fa8bddd6ef18522e30c763af5df60d9
Jiang Yuxin <jyx11011@gmail.com>
2020-09-19 18:01:49 +0800

    add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import { App, View } from 'framework7-react';
-import './App.css';
-import Details from './pages/Details';
+import React from "react";
+import { App, View } from "framework7-react";
+import "./App.css";
+import Details from "./pages/Details";
+import LoginPage from "./pages/Login/index.js";
 
 const f7params = {
-  root: '#app', // App root element
-  id: 'io.framework7.testapp', // App bundle ID
-  name: 'Framework7', // App name
-  theme: 'auto', // Automatic theme detection
-  // App routes
-  routes: [
-    {
-      path: '/details/',
-      component: Details,
-    },
-  ],
+    root: "#app", // App root element
+    id: "io.framework7.testapp", // App bundle ID
+    name: "Framework7", // App name
+    theme: "auto", // Automatic theme detection
+    // App routes
+    routes: [
+        {
+            path: "/details/",
+            component: Details,
+        },
+        {
+            path: "/login/",
+            component: LoginPage,
+        },
+    ],
 };
 
 function Main() {
-  return (
-    <App params={f7params}>
-      <View main url="/details/" />
-    </App>
-  );
+    return (
+        <App params={f7params}>
+            <View main url="/login/" />
+        </App>
+    );
 }
 
 export default Main;
