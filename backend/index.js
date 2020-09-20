466b2fc069ef1a410b187bd8cb0669eb5be42378
dawo5010 <me@dawo.me>
2020-09-20 10:40:39 +0800

    Finished toilet route.

--- backend/index.js
+++ backend/index.js
@@ -10,19 +10,72 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello Agnes!"));
 
+
 app.get("/toilets", async (req, res) => {
+
+    let rows;
+
     let statement = (SQL `
+    SELECT * 
+    FROM buildings`);
+
+    try {
+        let result = await db.query(statement);
+        rows = result.rows;
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+    
+    let buildings = {};
+
+    for (row in rows) {
+        let current = rows[row]
+        let building = {
+            buildingId: current.id,
+            buildingName: current.name,
+            region: current.region,
+            lat: current.latitude,
+            lon: current.longitude,
+            toilets: new Array(),
+        };
+
+        buildings[building.buildingId] = building;
+    }
+
+    console.log(buildings[1]);
+
+    statement = (SQL `
     SELECT *
     FROM ToiletSummary`);
 
 
     try {
-        const { rows } = await db.query(statement);
-        console.log(rows)
-        res.status(200).send(rows);
+        let result = await db.query(statement);
+        rows = result.rows
     } catch (error) {
-        res.status(500).send(error)
+        return res.status(500).send(error);
     }
+
+    for (row in rows) {
+        let current = rows[row];
+        console.log(current);
+        let toilet = {
+            toiletId: current.id,
+            name: current.name,
+            avg_review: (current.avg_review || 0),
+            review_count: (current.review_count || 0),
+        };
+
+        let toilets = buildings[current.building_id].toilets;
+        
+        if (toilets) {
+            toilets.push(toilet);
+        }
+    }
+
+    return res.status(200).send(Object.values(buildings));
+
 
 });
 
