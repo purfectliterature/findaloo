8e18adcf8c2ce8daca51443d9818621b2345ed8a
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-09-21 22:07:21 +0800

    Integrated ToiletCard for toilets grid view

--- frontend/src/components/ToiletCard/index.js
+++ frontend/src/components/ToiletCard/index.js
@@ -1,10 +1,88 @@
 import React from 'react';
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import MoneyOffIcon from "@material-ui/icons/MoneyOff";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 import "./styles.css";
 
+import Rating from "../Rating";
+import Feature from "../Feature";
+
 export default (props) => {
+    const {
+        ratingCount,
+        reviewRating,
+        hasBidet,
+        hasToiletPaper,
+        isMaleToilet,
+        isFemaleToilet,
+        isFree,
+        distance,
+        name,
+        image
+    } = props.toilet;
+
+    const renderMaleIcon = () => {
+        if (isMaleToilet) return (
+            <img alt="male" className="gender-icon" src={require("../../assets/icon-male.svg")} />
+        );
+    }
+
+    const renderFemaleIcon = () => {
+        if (isFemaleToilet) return (
+            <img alt="female" className="gender-icon" src={require("../../assets/icon-female.svg")} />
+        );
+    }
+
+    const renderSubtitle = () => (
+        <div className="card-subtitle">
+            <p>{distance}</p>
+
+            <div className="card-subtitle-dot" />
+
+            {isFree ? <MoneyOffIcon /> : <AttachMoneyIcon />}
+        </div>
+    );
+
+    const renderFeatures = () => (
+        <div className="features">
+            <Feature
+                featureName="Bidet"
+                has={hasBidet}
+                className="feature-lists"
+            />
+
+            <Feature
+                featureName="Bombs"
+                has={hasToiletPaper}
+                className="feature-lists"
+            />
+            
+            <Feature
+                featureName="Toilet paper"
+                has={hasToiletPaper}
+                className="feature-lists"
+            />
+        </div>
+    );
+
     return (
         <div className="toil-card">
-            {props.children}
+            <div className="card-image" style={{ backgroundImage: `url(${image})` }}>
+                <div className="gender-icon-container">
+                    {renderMaleIcon()}
+                    {renderFemaleIcon()}
+                </div>
+            </div>
+
+            <div className="card-info">
+                <h3>{name}</h3>
+
+                {renderSubtitle()}
+
+                <Rating rating={reviewRating} count={ratingCount} />
+
+                {renderFeatures()}
+            </div>
         </div>
     );
 }
--- frontend/src/pages/Explore/index.js
+++ frontend/src/pages/Explore/index.js
@@ -20,10 +20,67 @@ const buildings = [
 ];
 
 const toilets = [
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    "It is a long established fact that a reader will be distracted by the readable content of a.",
-    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+    {
+        image:
+            "https://www.alsco.com.sg/wp-content/uploads/2016/09/alsco-sg-greenroom-9most-overlooked-washroom-design-details-and-why-you-should-care.jpg",
+        name: "NUS LT27",
+        distance: "100m",
+        isFree: true,
+        isMaleToilet: true,
+        isFemaleToilet: true,
+        reviewRating: 3.8,
+        ratingCount: 1000,
+        hasBidet: true,
+        hasToiletPaper: true,
+    },
+    {
+        image:
+            "https://www.alsco.com.sg/wp-content/uploads/2016/09/alsco-sg-greenroom-9most-overlooked-washroom-design-details-and-why-you-should-care.jpg",
+        name: "NUS LT27",
+        distance: "100m",
+        isFree: true,
+        isMaleToilet: true,
+        isFemaleToilet: true,
+        reviewRating: 3.8,
+        ratingCount: 1000,
+        hasBidet: true,
+        hasToiletPaper: true,
+    },
+    {
+        image:
+            "https://www.alsco.com.sg/wp-content/uploads/2016/09/alsco-sg-greenroom-9most-overlooked-washroom-design-details-and-why-you-should-care.jpg",
+        name: "NUS LT27",
+        distance: "100m",
+        isFree: true,
+        isMaleToilet: true,
+        isFemaleToilet: true,
+        reviewRating: 3.8,
+        ratingCount: 1000,
+        hasBidet: true,
+        hasToiletPaper: true,
+    },
+    {
+        image:
+            "https://www.alsco.com.sg/wp-content/uploads/2016/09/alsco-sg-greenroom-9most-overlooked-washroom-design-details-and-why-you-should-care.jpg",
+        name: "NUS LT27",
+        distance: "100m",
+        isFree: true,
+        isMaleToilet: true,
+        isFemaleToilet: true,
+        reviewRating: 3.8,
+        ratingCount: 1000,
+        hasBidet: true,
+        hasToiletPaper: true,
+    }
 ];
+
+const renderBuildings = () => buildings.map((building) => (
+    <BuildingCard key={Math.random()} title={building.name} onClick={() => alert(building.name)} />
+));
+
+const renderToilets = () => toilets.map((toilet) => (
+    <ToiletCard key={Math.random()} toilet={toilet} />
+));
 
 export default (props) => {
     const bottomSheetRef = useRef();
