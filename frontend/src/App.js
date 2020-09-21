08aacb556002d2b7657a6fd2cc27deae5384c8c6
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-09-21 15:34:32 +0800

    (WIP) Fixed viewport erratic resizing on Android
    Need to remember initial screen height and apply onresize. Corner case:
    user refreshes page while keyboard is open, then the initialHeight will
    be cut by the keyboard.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { App } from 'framework7-react';
 import './App.css';
 
 import Explore from "./pages/Explore";
 
 export default (props) => {
+    useEffect(() => {
+        let viewport = document.querySelector("meta[name=viewport]");
+        viewport.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0");
+        const initialHeight = window.innerHeight;
+
+        window.onresize = () => {
+            const metaViewport = document.querySelector("meta[name=viewport]");
+
+            if (window.innerHeight < initialHeight) {
+                document.documentElement.style.setProperty("overflow", "auto");
+                metaViewport.setAttribute("content", `height=${initialHeight}px, width=device-width, initial-scale=1.0`);
+            } else {
+                document.documentElement.style.setProperty("overflow", "hidden");
+                viewport.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0");
+            }
+        }
+    }, []);
+
     return (
         <App>
             <Explore />
