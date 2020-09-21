c38f22bc7c4d761c152bdeb144c064470deeb293
dawo5010 <me@dawo.me>
2020-09-21 23:42:37 +0800

    Added auth_type to /customer/profile.

--- backend/sql/6_customersummary.sql
+++ backend/sql/6_customersummary.sql
@@ -1,7 +1,7 @@
 DROP VIEW IF EXISTS CustomerSummary;
 
-CREATE VIEW CustomerSummary(id, name, email, profile_picture, points) AS (
-    SELECT CP.user_id, CP.name, U.email, CP.profile_picture, CP.points
+CREATE VIEW CustomerSummary(id, name, email, auth_type, profile_picture, points) AS (
+    SELECT CP.user_id, CP.name, U.email, U.auth_type, CP.profile_picture, CP.points
     FROM customer_profiles CP 
     JOIN users U
     ON CP.user_id = U.id
