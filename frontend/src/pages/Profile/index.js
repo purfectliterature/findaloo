b888b04cce1ceba382a1e05d0870e468e67eae80
Jin Ying <kenneth.jyt@gmail.com>
2020-09-22 09:30:47 +0800

    Profile Page: Add user actions section

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Page, Navbar, Button } from 'framework7-react';
+import { Page, Navbar, Button, f7 } from 'framework7-react';
 import UserInfo from '../../components/UserInfo';
+import UserActions from '../../components/UserActions';
 import './styles.css';
 
 const Profile = () => {
   const userInfoStub = {
+    id: 1,
     name: 'Yuen Jian',
     profilePicture: 'https://www.comp.nus.edu.sg/stfphotos/sooyj_2.jpg',
     email: 'sooyj@comp.nus.edu.sg',
@@ -19,13 +21,54 @@ const Profile = () => {
     setUserInfo(userInfoStub);
   }, []);
 
+  const handleEditProfileOnClick = () => {
+    f7.views.main.router.navigate('/edit-profile/', {
+      props: {
+        userId: userInfo.id,
+        userName: userInfo.name,
+        userProfilePicture: userInfo.profilePicture,
+      },
+    });
+  };
+
+  const handleChangePasswordOnClick = () => {
+    f7.views.main.router.navigate('/change-password/', {
+      props: {
+        userId: userInfo.id,
+      },
+    });
+  };
+
+  const handleRewardsOnClick = () => {
+    f7.views.main.router.navigate('/rewards/', {
+      props: {
+        userId: userInfo.id,
+        points: userInfo.points,
+      },
+    });
+  };
+
+  const handleManageReviewsOnClick = () => {
+    f7.views.main.router.navigate('/manage-reviews/', {
+      props: {
+        userId: userInfo.id,
+      },
+    });
+  };
+
   return (
     <Page className="white-background-skin">
       <Navbar backLink />
 
-      <div className="margin display-flex flex-direction-column justify-content-space-between">
+      <div className="margin display-flex flex-direction-column justify-content-space-between profile-page">
         <div>
           <UserInfo userInfo={userInfo} />
+          <UserActions
+            handleEditProfileOnClick={handleEditProfileOnClick}
+            handleChangePasswordOnClick={handleChangePasswordOnClick}
+            handleRewardsOnClick={handleRewardsOnClick}
+            handleManageReviewsOnClick={handleManageReviewsOnClick}
+          />
         </div>
         <Button
           outline
