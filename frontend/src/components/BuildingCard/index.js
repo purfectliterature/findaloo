454289052d24bb6b3a200dd3128a9eb6251658a3
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-09-24 01:40:14 +0800

    Added image on BuildingCard

--- frontend/src/components/BuildingCard/index.js
+++ frontend/src/components/BuildingCard/index.js
@@ -4,11 +4,24 @@ import "./styles.css";
 export default (props) => {
     const {
         title,
-        onClick
+        onClick,
+        image
     } = props;
 
     return (
-        <div className="bldg-card ripple" onClick={onClick}>
+        <div
+            className="bldg-card ripple"
+            onClick={onClick}
+            style={{
+                background: `
+                    linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 40%, rgba(0,0,0,0) 40%, rgba(0,0,0,0.8) 100%),
+                    url("${image}"),
+                    #828282
+                `,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover"
+            }}
+        >
             <h3>{title}</h3>
         </div>
     )
