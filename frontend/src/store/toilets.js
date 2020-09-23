baa5c564c3073858b4a12dbf0a988a5a115f0dd9
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-09-23 22:17:15 +0800

    Added toilet details reducer

--- frontend/src/store/toilets.js
+++ frontend/src/store/toilets.js
@@ -10,19 +10,30 @@ const slice = createSlice({
     reducers: {
         buildingsAdded: (toilets, action) => {
             toilets.buildings = action.payload;
+        },
+
+        toiletAdded: (toilets, action) => {
+            toilets.toilets[action.payload.id] = action.payload.data;
         }
     }
 });
 
 const {
-    buildingsAdded
+    buildingsAdded,
+    toiletAdded
 } = slice.actions;
 
 export default slice.reducer;
 
 export const addBuildings = (buildings) => buildingsAdded(buildings);
+export const addToilet = (id, data) => toiletAdded({ id, data });
 
 export const getBuildings = createSelector(
     state => state.toilets,
     toilets => toilets.buildings
+);
+
+export const getToiletDetails = (id) => createSelector(
+    state => state.toilets,
+    toilets => toilets.toilets[id]
 );
