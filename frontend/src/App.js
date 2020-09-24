1546ceb071e752f451c8cbeec70ea7c8c8a26c4d
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-09-25 04:48:30 +0800

    Removed unused viewport modifier for soft keyboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -83,36 +83,6 @@ const f7params = {
 };
 
 export default (props) => {
-    useEffect(() => {
-        let viewport = document.querySelector("meta[name=viewport]");
-        viewport.setAttribute(
-            "content",
-            "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        );
-        const initialHeight = window.innerHeight;
-
-        window.onresize = () => {
-            const metaViewport = document.querySelector("meta[name=viewport]");
-
-            if (window.innerHeight < initialHeight) {
-                document.documentElement.style.setProperty("overflow", "auto");
-                metaViewport.setAttribute(
-                    "content",
-                    `height=${initialHeight}px, width=device-width, initial-scale=1.0`
-                );
-            } else {
-                document.documentElement.style.setProperty(
-                    "overflow",
-                    "hidden"
-                );
-                viewport.setAttribute(
-                    "content",
-                    "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-                );
-            }
-        };
-    }, []);
-
     return (
         <Provider store={store}>
             <PersistGate loading={<FetchLoading />} persistor={persistor}>
