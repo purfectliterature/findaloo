73f6d5acebdd1f73c2ac70256e53d7d631cd9c4b
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-09-25 22:25:04 +0800

    Removed redundant useEffect from home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { App, View } from "framework7-react";
 import ReactGA from "react-ga";
 import { Provider } from "react-redux";
