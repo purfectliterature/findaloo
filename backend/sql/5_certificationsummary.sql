be1ed0d6fb212858afd04f625c6da571d083a4ee
dawo5010 <me@dawo.me>
2020-09-25 17:53:21 +0800

    updated readme.

--- backend/sql/5_certificationsummary.sql
+++
@@ -1,8 +0,0 @@
-DROP VIEW IF EXISTS CertificationSummary;
-
-CREATE VIEW CertificationSummary(toilet_id, certification_authority, certification_logo, certification_webpage, rating) AS (
-    SELECT TC.toilet_id, CA.certification_authority, CA.certification_logo, CA.certification_webpage, TC.rating
-    FROM toilet_certifications TC
-    JOIN certification_authorities CA
-    ON TC.certification_id = CA.id
-);
--- backend/sql/6_customersummary.sql
+++
@@ -1,8 +0,0 @@
-DROP VIEW IF EXISTS CustomerSummary;
-
-CREATE VIEW CustomerSummary(id, name, email, auth_type, profile_picture, points) AS (
-    SELECT CP.user_id, CP.name, U.email, U.auth_type, CP.profile_picture, CP.points
-    FROM customer_profiles CP 
-    JOIN users U
-    ON CP.user_id = U.id
-)
